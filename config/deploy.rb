ef4dd4dc197a8d2de5128eb7e886530f1d88cf7b
Tarun Jadhwani <tarunjadhwani@gmail.com>
2014-01-03 15:14:24 +0530

    deploy:restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'structwares'
 set :repository, 'git@github.com:tarunjadhwani/structwares.git'
 set :scm, :git
-set :deploy_via, :copy
+set :deploy_via, :remote_cache
 set :deploy_to, "www/test"
 set :use_sudo, false
 set :branch, 'master'
@@ -12,3 +12,9 @@ set :user, "equicomi"
 role :web, "test.structwares.com"
 role :app, "test.structwares.com"
 role :db, "test.structwares.com", :primary => true
+
+after 'deploy:update_code', 'deploy:migrate'
+
+set :keep_releases, 5
+
+after "deploy:restart", "deploy:cleanup" 
